93db7ef36f535b669355e91674247e58ffb6a091
Dipak Pawar <itsdipakpawar4206@gmail.com>
2022-12-02 00:20:27 +0530

    add files

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { SignupApi } from "../ReduxStore/Auth/auth.action";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+const [SignupCred,setSignupCred]=useState({})
+
+const dispatch=useDispatch()
+
+
+const handeldchange=(e)=>{
+const {name,value}=e.target
+setSignupCred({
+  ...SignupCred,[name]:value
+})
 }
 
-export default Signup
+const handeldsubmit=(e)=>{
+e.preventDefault()
+dispatch(SignupApi(SignupCred))
+
+console.log(SignupCred)
+
+}
+  
+  return (
+    <div>
+      <form onSubmit={handeldsubmit}>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          name="name"
+          onChange={handeldchange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter UserName"
+          name="username"
+          onChange={handeldchange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          onChange={handeldchange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter Password"
+          name="password"
+          onChange={handeldchange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter Mobile"
+          name="mobile"
+          onChange={handeldchange}
+        />
+        <br />
+        <select name="country" onChange={handeldchange}>
+          <option value="india">India</option>
+          <option value="usa">USA</option>
+          <option value="china">CHINA</option>
+          <option value="russia">RUSSIA</option>
+        </select>
+        <br />
+
+        <select name="gender" onChange={handeldchange}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="Unspecifide">Unspecifide</option>
+        </select>
+
+        <input type="submit" value="Signup" />
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
