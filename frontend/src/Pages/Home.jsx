b57aea03a2764da95379f4a549398d4dcb5e06bc
Dipak Pawar <itsdipakpawar4206@gmail.com>
2022-12-07 15:55:12 +0530

    added the comment section in omepahe

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,38 +1,63 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Getallpost } from '../ReduxStore/Feeds/feeds.action'
-import "../CompliedSassCss/css/Home.css"
-const Home = () => {
-const dispatch=useDispatch()
-const data=useSelector((store)=>store.Feeds.data)
+import React from "react";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {Getallpost} from "../ReduxStore/Feeds/feeds.action";
+import "../CompliedSassCss/css/Home.css";
+import { BsSuitHeart } from 'react-icons/bs';
+import { FaRegComment } from 'react-icons/fa';
+import { FiSend } from 'react-icons/fi';
+import { useState } from "react";
+import Tooltip from '@mui/material/Tooltip';
 
-useEffect(()=>{
-dispatch(Getallpost())
-},[])
+const Home = () => {
+  const [isopen,setisopen]=useState(true)
+
+const opencomment=()=>{
+setisopen(isopen?false:true)
+}
+  const dispatch = useDispatch();
+  const data = useSelector((store) => store.Feeds.data);
+
+  useEffect(() => {
+    dispatch(Getallpost());
+  }, []);
 
   return (
-    <div>
-      {/* {data.map((el)=><h1>{el.title}</h1>)} */}
-<div className='feeddiv'>
+    <div className="homepagediv">
+      {data.map((el) => (
+        <div className="feeddiv">
+          <div className="feeduserinfo">
+            <img
+              src="https://avatars.githubusercontent.com/u/96649241?v=4"
+              alt=""
+            />
+            <p>dipak</p>
+          </div>
 
-<div className='feeduserinfo'>
+          <div className="feedimg">
+            <img src={`http://localhost:8100/static/${el.imagepath}`} alt="" />
+          </div>
 
-<img  src="https://avatars.githubusercontent.com/u/96649241?v=4" alt="" />
-<p>dipak</p>
-</div>
+          <div className="feedbottom">
+            <div className="icondiv">
+            <BsSuitHeart className="likeicon"/>
+            <Tooltip title="Comment" arrow>
+            <FaRegComment className="commenticon" onClick={opencomment}/>
+</Tooltip>
 
-<div className='feedimg'>
+            </div>
 
-</div>
-<div className='feedbottom'>
 
-</div>
-
-</div>
+{isopen?
+<div className="addcommentdiv">
+  <input type="text" placeholder="Write Comment Heare" />
+  <FiSend className="addcommentbtn"/>
+</div>:""}
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
